b8508cb810546dca1692a5c61904354a591db4ec
Henthorn Lab @ RHIT <henthor1@rose-hulman.edu>
2022-08-21 13:07:21 -0400

    Minor fixes

--- labarea.py
+++ labarea.py
@@ -7,7 +7,7 @@ These are the permitted areas that can be queried.
 class LabArea:
     def __init__(self, area_num):
         # These are the permitted areas. Add them as a key pair with area number:search term
-        # For example, all tags on are 150 would fit the pattern of *150*, so the key pair is '150':'*150*'
+        # For example, all tags on area 150 would fit the pattern of *150*, so the key pair is '150':'*150*'
         self.allowed_areas = {'100': '*100*', '150': '*150*', '200': '*200*', '250': '*250*', '300': '*-3*',
                               '400': '*400*', '500': '*500*', '600': '*600*', '700': '*700*', '800': '*800*'}
 
